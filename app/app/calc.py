c618592d33476a216a608545bebc8629a42d0df1
Tomas <tcornejo@fen.uchile.cl>
2022-06-20 00:32:04 -0400

    Borrados espacios en blanco por formato

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,8 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x and y and return result"""
     return x + y
-    
-    
+
+
 def substract(x, y):
     """Substract x and y return result"""
     return x - y
