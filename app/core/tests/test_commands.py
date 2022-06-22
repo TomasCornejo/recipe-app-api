a9fd8b76958c695ca8b98683a3f77cabc18d24ae
Tomas <tcornejo@fen.uchile.cl>
2022-06-22 00:14:32 -0400

    Configurado workflow for wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -18,7 +19,7 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
 
         call_command('wait_for_db')
-        
+
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
@@ -30,4 +31,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases='default')
+        patched_check.assert_called_with(databases=['default'])
